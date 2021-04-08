9698ab62c04bcdf2d5f9bb0bca04f698e21414ac
Kharizza Viloria <kharizzakaye@gmail.com>
2021-04-08 14:13:03 +0800

    update code

---
+++ script.js
@@ -0,0 +1,10 @@
+function compute()
+{
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+
+    
+}
+        
